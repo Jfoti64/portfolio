44ff957d807d537419960a8f266bd83be61c1bec
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-06-22 19:16:38 -0500

    Add links

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,11 +9,14 @@ const Intro = () => {
         I can help you bring your ideas to life with seamless and engaging user experiences.
       </p>
       <div className="flex space-x-4">
-        <a href="https://www.linkedin.com" className="flex flex-row items-center gap-1">
+        <a
+          href="https://www.linkedin.com/in/joshua-foti-23676b182/"
+          className="flex flex-row items-center gap-1"
+        >
           <FontAwesomeIcon icon={faLinkedin} className="w-5 h-5" />
           <p>LinkedIn</p>
         </a>
-        <a href="https://www.github.com" className="flex flex-row items-center gap-1">
+        <a href="https://github.com/Jfoti64" className="flex flex-row items-center gap-1">
           <FontAwesomeIcon icon={faGithub} className="w-5 h-5" />
           <p>GitHub</p>
         </a>

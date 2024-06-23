e9ecf85b77488a8b7fb5a2cc999c162ed3670a32
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-06-22 20:26:28 -0500

    Fix gap and styling on small and larger screens.

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import Projects from './components/Projects';
 
 function App() {
   return (
-    <div className="flex flex-col gap-44">
-      <Navbar />
-      <Intro />
-      <About />
-      <Projects />
+    <div className="flex flex-col items-center ">
+      <div className="flex flex-col gap-16 md:gap-24 lg:gap-32 w-full max-w-screen-lg">
+        <Navbar />
+        <Intro />
+        <About />
+        <Projects />
+      </div>
     </div>
   );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center text-center p-5 bg-customBackground min-h-screen gap-5">
+    <div className="flex flex-col md:flex-row items-center justify-center text-center p-5 bg-customBackground gap-5 w-full max-w-screen-lg">
       <div className="flex flex-col items-center md:w-2/3">
         <h1 className="text-4xl font-bold mb-4">Fullstack Developer</h1>
         <p className="text-xl mb-8 max-w-2xl">

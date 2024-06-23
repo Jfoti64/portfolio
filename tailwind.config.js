e23e892d179cc5534f39a69608ef85827f6d425b
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-06-22 20:34:54 -0500

    Add Projects section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
         customHover: '#A1A1A1',
         skillBox: '#f5f5f5', // Light grey for skill boxes
         skillBoxHover: '#e0e0e0', // Slightly darker grey for hover state
+        linkBox: '#333333', // Darker grey for links
+        linkBoxHover: '#1a1a1a', // Even darker grey for link hover state
       },
     },
   },

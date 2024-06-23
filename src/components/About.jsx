c8373c8ca3debffd99ef0b56dcdd4cf2d980c6b3
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-06-22 19:16:46 -0500

    Update styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,19 +3,19 @@ const About = () => {
     <div className="flex flex-col md:flex-row items-center justify-center text-center p-5 bg-customBackground min-h-screen gap-5">
       <div className="flex flex-col items-center md:w-2/3">
         <h1 className="text-4xl font-bold mb-4">Fullstack Developer</h1>
-        <p className="text-2xl mb-8 max-w-2xl">
+        <p className="text-xl mb-8 max-w-2xl">
           Hi! I&apos;m Joshua Foti, a Web Developer based in Panama City, FL. I specialize in
           building seamless user experiences using technologies like JavaScript, React, and Node.js.
           My skills in database management with MongoDB and SQL, along with experience in version
           control, testing, and deployment, equip me to tackle any web development project.
         </p>
-        <p className="text-2xl mb-8 max-w-2xl">
+        <p className="text-xl mb-8 max-w-2xl">
           I thrive in collaborative environments and seek opportunities to grow as a developer
           within a team that values creativity, innovation, and excellence.
         </p>
-        <p className="text-2xl mb-8 max-w-2xl">
+        <p className="text-xl mb-8 max-w-2xl">
           Outside of work, you&apos;ll find me watching hockey, exercising, or indulging in my love
-          for film and traveling. Let&apos;s connect and build something amazing!
+          for film and traveling. Let&apos;s connect and start building.
         </p>
       </div>
       <div className="flex flex-col items-center md:w-1/3 mt-8 md:mt-0 space-y-4">
